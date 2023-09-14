a5e1889a1b4c6967bfb6838ee5f51f24ffbbb235
Kaitlyn Williamson <kaitlyn.shae28@gmail.com>
2023-09-13 20:41:52 -0400

    y

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function findMatching(names, nameWanted){
     return names.filter((name) => name.toLowerCase() === nameWanted.toLowerCase())
 }
 
+function fuzzyMatch(names, letter){
+    return names.filter((name) => name.startsWith(letter))
+}
+
+function matchName(drivers, data){
+    return drivers.filter(driver => driver.name === data)
+}
